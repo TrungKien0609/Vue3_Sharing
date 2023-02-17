f779c8515658ad027b1ac075a035d25fdf6fe22b
Trung Kien <trungkien7300@gmail.com>
2023-02-17 17:57:57 +0700

    try debug

--- vue2/src/mixins/otherMixin.ts
+++ vue2/src/mixins/otherMixin.ts
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 export default Vue.extend({
   methods: {
-    resetCurrentEmployeeMixin() {
-      console.log('helllo')
-    }
+    // resetCurrentEmployeeMixin() {
+    //   console.log('helllo')
+    // }
   }
 })
